5d3726195faf0561a3feb5eb551a4db671e900a9
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-07-25 10:21:04 +0545

    Feat: format key function

--- core/logger.py
+++ core/logger.py
@@ -4,6 +4,10 @@ from pynput import keyboard
 from config import LOG_FILE
 from datetime import datetime
 
+def format_key(key):
+    """Format special keys to be readable"""
+    pass
+
 def write_log(key):
     pass
 
