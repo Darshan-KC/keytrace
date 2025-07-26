bd5a025ae685a585c4abd414a899c7af54d2efe4
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-07-26 20:50:44 +0545

    Feat: Logger format key update

--- core/logger.py
+++ core/logger.py
@@ -6,7 +6,29 @@ from datetime import datetime
 
 def format_key(key):
     """Format special keys to be readable"""
-    pass
+    try:
+        return key.char  # For regular characters
+    except AttributeError:
+        special_keys = {
+            keyboard.Key.space: ' ',
+            keyboard.Key.enter: '\n',
+            keyboard.Key.tab: '[TAB]',
+            keyboard.Key.backspace: '[BACKSPACE]',
+            keyboard.Key.shift: '[SHIFT]',
+            keyboard.Key.shift_r: '[SHIFT]',
+            keyboard.Key.ctrl_l: '[CTRL]',
+            keyboard.Key.ctrl_r: '[CTRL]',
+            keyboard.Key.alt_l: '[ALT]',
+            keyboard.Key.alt_r: '[ALT]',
+            keyboard.Key.esc: '[ESC]',
+            keyboard.Key.delete: '[DEL]',
+            keyboard.Key.caps_lock: '[CAPSLOCK]',
+            keyboard.Key.up: '[UP]',
+            keyboard.Key.down: '[DOWN]',
+            keyboard.Key.left: '[LEFT]',
+            keyboard.Key.right: '[RIGHT]'
+        }
+        return special_keys.get(key, f'[{key.name.upper()}]')
 
 def write_log(key):
     pass
