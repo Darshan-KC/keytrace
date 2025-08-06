8dcd99e907c9e1946c3011343eca7fd9d7ded19a
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-08-06 20:10:52 +0545

    Feat: run in background update

--- run_hidden.py
+++ run_hidden.py
@@ -4,4 +4,7 @@ import os
 import platform
 
 def run_in_background():
+    system = platform.system()
+    script_path = os.path.join(os.path.dirname(__file__), "main.py")
+    base_cmd = [sys.executable, script_path, "--silent"]
     pass
