0591c67f155c0625771ac288bdb6cd0d7857f3db
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-08-15 16:42:48 +0545

    Feat: test main file added

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+import os
+import signal
+import time
+
+MAIN_PATH = os.path.join(os.path.dirname(__file__), "..", "main.py")
+
+def test_main_silent_mode_launch():
+    pass
