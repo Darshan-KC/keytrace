1ed8641c289e303414776721f9b020bba1736f00
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-08-17 21:58:37 +0545

    Feat: test run hidden executes

---
+++ tests/test_run_hidden.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+import os
+import time
+
+RUN_HIDDEN_PATH = os.path.join(os.path.dirname(__file__), "..", "run_hidden.py")
+
+def test_run_hidden_executes():
+    pass
